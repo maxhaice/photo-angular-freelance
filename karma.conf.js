fb237a155250f4dc004d43bb0494509188647870
Eugene Ostash <jenya.ostash@gmail.com>
2021-06-12 23:21:42 +0300

    update

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/image-wrapper'),
+      dir: require('path').join(__dirname, './coverage/photo-angular-freelance'),
       subdir: '.',
       reporters: [
         { type: 'html' },
