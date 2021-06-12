173bb4d9cf7a4d6accc3926e4f87ca0b8e7ea8b1
Eugene Ostash <jenya.ostash@gmail.com>
2021-06-12 23:36:42 +0300

    new update

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
+//Install express server
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+// Serve only the static files form the dist directory
 app.use(express.static('./dist/photo-angular-freelance'));
 
-app.get('/*', function (req, res) {
-  res.sendFile('index.html', { root: 'dist/photo-angular-freelance' }
-  );
-});
+app.get('/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/photo-angular-freelance/'}),
+);
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
-console.log(`Running on port ${process.env.PORT || 8080}`)
